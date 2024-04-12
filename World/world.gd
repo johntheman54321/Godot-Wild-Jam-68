07ad5fd1dda6ea891beadcd7b3b54467520d6540
Moneooo <166874209+Moneooo@users.noreply.github.com>
2024-04-12 18:09:55 -0400

    Added

--- World/world.gd
+++ World/world.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-var time = 0
+var hour = 0
+var day = 0
 
 func _process(delta):
-	$Timer_UI.text = str(time)
-
-
+	$Timer_UI.text = "Hour " + str(hour)
 
 func _on_timer_timeout():
-	time += 1
-	
+	hour += 1
+	if hour == 24:
+		hour = 0
+		day += 1
