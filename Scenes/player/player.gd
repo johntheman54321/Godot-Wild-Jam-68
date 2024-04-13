a89cdb1bb5516a2a14e8713f8e98bb52e6d63270
Moneooo <166874209+Moneooo@users.noreply.github.com>
2024-04-13 19:00:44 -0400

    Fixed diagonal movement

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -4,5 +4,5 @@ extends CharacterBody2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	position += Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * speed * delta
+	position += Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized() * speed * delta
 	move_and_slide()
