6b5fa68f0620aa5c1bb72ed42c96034612f0462c
Moneooo <166874209+Moneooo@users.noreply.github.com>
2024-04-13 13:38:10 -0400

    Character

---
+++ Scenes/player/player.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export var speed = 3
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * speed
