66a9729670b9fb3316e5429b3619d3abc1776814
johntheman54321 <john.wahba09@gmail.com>
2024-04-12 23:48:33 -0400

    Made fade in and fade out

--- Scripts/loading_screen.gd
+++ Scripts/loading_screen.gd
@@ -1,4 +1,15 @@
 extends Node2D
 
+#Make the var in for fade in and out for fade out
+@export var animation_type = ""
+
 func _ready():
-	$AnimationPlayer.play("Fade")
+	if animation_type == "in":
+		$AnimationPlayer.play("Fade in")
+	elif animation_type == "out":
+		$AnimationPlayer.play("Fade out")
+
+func _process(delta):
+	if $AnimationPlayer.is_playing() == true:
+		print("playing")
+	
