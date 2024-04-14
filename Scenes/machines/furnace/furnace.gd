6ffec2768bb0e7f8b45968a816c1d3ff059de786
Moneooo <166874209+Moneooo@users.noreply.github.com>
2024-04-14 19:58:57 -0400

    Added some of the furnace logic

---
+++ Scenes/machines/furnace/furnace.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+@export var player:CharacterBody2D
+var playerIsClose = false
+var isCooking = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("Interact"):
+		$Tooltip.hide()
+		isCooking = true
+
+func _on_area_2d_body_entered(body):
+	if body == player and not isCooking:
+		$Tooltip.show()
+
+func _on_area_2d_body_exited(body):
+	if body == player:
+		$Tooltip.hide()
