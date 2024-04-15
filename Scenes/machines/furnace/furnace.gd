5d273013189ce95a70da1784a83600ada8177557
johntheman54321 <john.wahba09@gmail.com>
2024-04-15 19:45:05 -0400

    Added SFX

--- Scenes/machines/furnace/furnace.gd
+++ Scenes/machines/furnace/furnace.gd
@@ -1,7 +1,6 @@
 extends StaticBody2D
 
 @export var player:CharacterBody2D
-var playerIsClose = false
 var isCooking = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -10,6 +9,8 @@ func _process(delta):
 		$Tooltip.hide()
 		isCooking = true
 		$AnimatedSprite2D.play("1-lit")
+		if isCooking == true:
+			$Cooking_SFX/AudioStreamPlayer2D.play()
 
 func _on_area_2d_body_entered(body):
 	if body == player and not isCooking:
@@ -19,3 +20,13 @@ func _on_area_2d_body_exited(body):
 	if body == player:
 		$Tooltip.hide()
 		
+
+
+func _on_cooking_sfx_body_entered(body):
+	if body == player and isCooking:
+		$Cooking_SFX/AudioStreamPlayer2D.play()
+
+
+func _on_cooking_sfx_body_exited(body):
+	if body == player and isCooking:
+		$Cooking_SFX/AudioStreamPlayer2D.stop()
