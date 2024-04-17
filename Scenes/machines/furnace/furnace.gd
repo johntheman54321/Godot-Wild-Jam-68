cbfcad09ea7603fa883ffd17d9112badbe21dd78
Moneooo <166874209+Moneooo@users.noreply.github.com>
2024-04-17 18:42:22 -0400

    Npc progression
    - Npcs now walk in every hour
    - Also fixed furnace sprite (furnace_lit animation was one pixel higher than furnace_unlit)

--- Scenes/machines/furnace/furnace.gd
+++ Scenes/machines/furnace/furnace.gd
@@ -2,13 +2,14 @@ extends StaticBody2D
 
 @export var player:CharacterBody2D
 var isCooking = false
+var isPlayerInRange = false
 
 func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("Interact"):
+	if Input.is_action_just_pressed("Interact") and isPlayerInRange:
 		$Tooltip.hide()
 		isCooking = true
 		$AnimatedSprite2D.play("1-lit")
@@ -19,17 +20,16 @@ func _process(delta):
 func _on_area_2d_body_entered(body):
 	if body == player and not isCooking:
 		$Tooltip.show()
+		isPlayerInRange = true
 
 func _on_area_2d_body_exited(body):
 	if body == player:
 		$Tooltip.hide()
-		
-
+		isPlayerInRange = false
 
 func _on_cooking_sfx_body_entered(body):
 	if body == player and isCooking:
 		$Cooking_SFX/AudioStreamPlayer2D.play()
-
 
 func _on_cooking_sfx_body_exited(body):
 	if body == player and isCooking:
