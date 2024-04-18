5f678e30c7b203c4d45202e606abbb8df2995627
johntheman54321 <john.wahba09@gmail.com>
2024-04-18 16:35:32 -0400

    Finished shopkeeper numbers

--- Scenes/ShopKeeper/shop_keeper.gd
+++ Scenes/ShopKeeper/shop_keeper.gd
@@ -1,10 +1,10 @@
 extends StaticBody2D
 
 @export var player : CharacterBody2D
-var Quantity = 0
+var quantity = 0
 
 func _process(delta):
-	$OrderMenu/Label.text = str(Quantity)
+	$OrderMenu/Label.text = str(quantity)
 
 func _on_area_2d_body_entered(body):
 	if body == player:
@@ -14,15 +14,29 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	$AnimatedSprite2D.play("Non-Interactable")
 	$OrderMenu.hide()
-	Quantity = 0
+	quantity = 0
 
 #Quantity
 func _on_more_pressed():
-	if MOUSE_BUTTON_LEFT:
-		if Quantity < 100:
-			Quantity += 1
-		print("left")
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) == true:
+				if quantity < 100:
+					quantity += 1
+					print("left")
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) == true:
+				if quantity < 91:
+					quantity += 10
+					print("right")
+	
 
 func _on_less_pressed():
-	if Quantity > 0:
-		Quantity -= 1
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) == true:
+		if quantity > 0:
+			quantity -= 1
+			print("left")
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) == true:
+		if quantity > 9:
+			quantity -= 10
+			print("right")
+	
+
+
