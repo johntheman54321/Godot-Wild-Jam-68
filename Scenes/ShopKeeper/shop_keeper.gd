a7a3cbf9a000d096d362db5018a4b80e3fff9a96
johntheman54321 <john.wahba09@gmail.com>
2024-04-17 21:21:51 -0400

    Working on buttons

--- Scenes/ShopKeeper/shop_keeper.gd
+++ Scenes/ShopKeeper/shop_keeper.gd
@@ -1,6 +1,10 @@
 extends StaticBody2D
 
 @export var player : CharacterBody2D
+var Quantity = 0
+
+func _process(delta):
+	$OrderMenu/Label.text = str(Quantity)
 
 func _on_area_2d_body_entered(body):
 	if body == player:
@@ -10,3 +14,15 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	$AnimatedSprite2D.play("Non-Interactable")
 	$OrderMenu.hide()
+	Quantity = 0
+
+#Quantity
+func _on_more_pressed():
+	if MOUSE_BUTTON_LEFT:
+		if Quantity < 100:
+			Quantity += 1
+		print("left")
+
+func _on_less_pressed():
+	if Quantity > 0:
+		Quantity -= 1
