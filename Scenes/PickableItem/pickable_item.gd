0e403eb9aed9f91c093a828f979d6f60942de165
Moneooo <166874209+Moneooo@users.noreply.github.com>
2024-04-19 18:06:29 -0400

    Finished item system

--- Scenes/PickableItem/pickable_item.gd
+++ Scenes/PickableItem/pickable_item.gd
@@ -1,23 +1,29 @@
 extends Node2D
 
 @export var player:CharacterBody2D
+@export var pickupOffsetRange = 2
 var isInteractable = false
 var isPickedUp = false
+var pickedUpOffset
 
 func _ready():
-	pass
+	randomize()
+	pickedUpOffset = Vector2(randi_range(-pickupOffsetRange, pickupOffsetRange), randi_range(-pickupOffsetRange, pickupOffsetRange))
 
 func _process(delta):
-	if isInteractable == true:
+	if isInteractable:
 		$AnimatedSprite2D.play("coal-interactable")
-		if isPickedUp == false:
+		if not isPickedUp:
 			if Input.is_action_just_pressed("Interact"):
 				isPickedUp = true
 				print("pickedup")
 				isInteractable = false
-	if isInteractable == false:
+	else:
 		$AnimatedSprite2D.play("coal")
-
+	
+	if isPickedUp:
+		global_position = player.get_child(2).global_position + pickedUpOffset
+	
 func _on_interactable_range_body_entered(body):
 	if body == player:
 		isInteractable = true
